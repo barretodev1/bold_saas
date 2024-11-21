f343e7f35c154212bf547c54fc1c00a52465ebc6
Caio Barreto <caiobarreto1acim@gmail.com>
2024-11-21 01:34:27 -0300

    fix: resolve a eslint bug of website

--- app/pages/website/page.tsx
+++ app/pages/website/page.tsx
@@ -1,4 +1,3 @@
-import HeaderComponent from "@/app/_components/header";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { CircleCheckBigIcon, CircleMinusIcon } from "lucide-react";

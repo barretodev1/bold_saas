d1679051457592fe437317bc7b9f4e0a75c74eb6
Caio Barreto <caiobarreto1acim@gmail.com>
2024-11-27 19:44:38 -0300

    feat: add a cookies page

---
+++ app/pages/cookies/page.tsx
@@ -0,0 +1,41 @@
+import HeaderComponent from "@/app/_components/header";
+
+const CookiesContratoPage = () => {
+    return ( 
+        <>
+        <main className="flex items-center justify-center flex-col p-14 gap-4">
+            <div className="flex flex-col gap-3">
+                <h1 className="text-2xl font-bold">Política de Cookies da Bold Company</h1>
+                <h2 className="font-semibold">Na Bold Company, a privacidade e a transparência são prioridades. Por isso, estamos comprometidos em proteger seus dados pessoais de acordo com a Lei Geral de Proteção de Dados (LGPD - Lei nº 13.709/2018).
+                    Utilizamos cookies em nosso site para melhorar sua experiência, personalizar conteúdos, analisar o desempenho e oferecer soluções alinhadas às suas preferências, como nossos serviços de Criação de Websites, Campanhas e Tráfego Pago, Social Media, Consultoria de Marketing, Design e Técnicas de Copywriting.
+                </h2>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <h1 className="text-2xl font-bold">O que são cookies?</h1>
+                <h2 className="font-semibold">
+                    Cookies são pequenos arquivos de texto armazenados no seu dispositivo quando você visita nosso site. Eles nos ajudam a entender como você interage com nosso conteúdo, lembrar suas preferências e aprimorar sua navegação.
+                </h2>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <h1 className="text-2xl font-bold">Consentimento e Gerenciamento de Cookies</h1>
+                <h2 className="font-semibold">
+                    Ao acessar nosso site, você pode optar por aceitar ou recusar cookies por meio do banner de consentimento. Além disso, é possível gerenciar suas preferências a qualquer momento nas configurações do navegador.                
+                </h2>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <h1 className="text-2xl font-bold">Fale Conosco</h1>
+                <h2 className="font-semibold">
+                    Caso tenha dúvidas sobre nossa Política de Cookies ou sobre como seus dados pessoais são tratados, entre em contato conosco pelo WhatsApp: 11 93325-5476.
+                    Estamos à disposição para garantir que sua experiência com a Bold Company seja segura, transparente e alinhada às normas de proteção de dados.
+                </h2>
+            </div>
+ 
+        </main>
+        </>
+     );
+}
+ 
+export default CookiesContratoPage;

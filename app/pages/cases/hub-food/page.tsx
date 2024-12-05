bafd034d4b2525a09e5517f0658eee5a79973aeb
Caio Barreto <caiobarreto1acim@gmail.com>
2024-12-05 17:22:17 -0300

    feat: add a hub food page

--- app/pages/cases/hub-food/page.tsx
+++ app/pages/cases/hub-food/page.tsx
@@ -23,7 +23,7 @@ const RarityPage = () => {
 
   return (
     <>
-      <HeaderCell/>
+      <HeaderCell />
 
       <div className="principal  relative flex flex-col items-center w-full justify-center overflow-hidden">
         <div className="flex px-4 ml-[3%] mt-6 absolute top-0 left-0 md:ml-[11%]">
@@ -39,10 +39,7 @@ const RarityPage = () => {
               </BreadcrumbItem>
               <BreadcrumbSeparator />
               <BreadcrumbItem>
-                <BreadcrumbLink
-                  className="text-white"
-                  href="/pages/cases"
-                >
+                <BreadcrumbLink className="text-white" href="/pages/cases">
                   Cases
                 </BreadcrumbLink>
               </BreadcrumbItem>
@@ -58,24 +55,25 @@ const RarityPage = () => {
 
         <div className="absolute px-4 flex flex-col mt-10 left-0 w-full hidden md:block">
           <h1 className="w-full flex font-bold text-white text-xl ml-[3%] md:ml-[11%] mt-16">
-            UHN Store: Tráfego Pago
+            Hub Food: Tráfego Pago & E-commerce
           </h1>
-          <h2 className="relative mb-20 left-0 text-sm' font-normal text-white mt-1 ml-[3%] md:ml-[11%]">
-          O mercado de moda fitness é altamente competitivo<br></br> no
-            Google. Para a UHN, a Bold Company utilizou<br></br> tráfego pago e
-            otimização de palavras-chave<br></br> genéricas para aumentar o
-            tráfego e guiar os usuários<br></br> em todas as etapas da jornada
-            de compra.
+          <h2 className="relative mb-20 left-0 text-sm font-normal text-white mt-1 ml-[3%] md:ml-[11%]">
+            O mercado de delivery de comida é altamente competitivo<br></br> nas
+            redes sociais. Para a Hub Food, a Bold Company<br></br> desenvolveu
+            uma estratégia focada em tráfego pago e<br></br> criação de um site
+            otimizado, aumentando a visibilidade e<br></br> guiando os clientes
+            em todas as etapas da jornada de compra.
           </h2>
+
           <h1 className="relative left-0 w-full flex font-extrabold text-white text-xl ml-[3%] md:ml-[11%]">
-            UHN Store
+            Hub Food
           </h1>
           <h2 className="relative left-0 mb-3 text-base font-normal text-white ml-[3%] md:ml-[11%]">
-            Loja de Roupa
+            Tráfego Pago & E-commerce
           </h2>
           <a
             data-aos="fade-right"
-            href="https://shopee.com.br/uhnstore?entryPoint=ShopBySearch&searchKeyword=uhn%20store"
+            href="https://famgz-food-hub-app.vercel.app/"
             className="bg-violet-600 mt-3 p-2 w-40 items-center justify-center rounded-full relative flex left-0 text-base font-semibold hover:bg-violet-950 transition-all text-white mt-1 ml-[3%] md:ml-[11%]"
           >
             Conferir
@@ -84,24 +82,24 @@ const RarityPage = () => {
 
         <div className="absolute px-4 flex flex-col mt-4 left-0 w-full block md:hidden">
           <h1 className="w-full flex font-bold text-white text-xl ml-[3%] md:ml-[11%]">
-            UHN Store: Tráfego Pago
+            Hub Food: Tráfego Pago & E-commerce
           </h1>
           <h2 className="relative mb-4 left-0 text-sm' font-normal text-white mt-1 ml-[3%] md:ml-[11%]">
-            O mercado de moda fitness é altamente competitivo no
-            Google. Para a UHN, a Bold Company utilizou tráfego pago e
-            otimização de palavras-chave genéricas para aumentar o
-            tráfego e guiar os usuários em todas as etapas da jornada
-            de compra.
+            O mercado de delivery de comida é altamente competitivo nas redes
+            sociais. Para a Hub Food, a Bold Company desenvolveu uma estratégia
+            focada em tráfego pago e criação de um site otimizado, aumentando a
+            visibilidade e guiando os clientes em todas as etapas da jornada de
+            compra.
           </h2>
           <h1 className="relative left-0 w-full flex font-extrabold text-white text-xl ml-[3%] md:ml-[11%]">
-            UHN Store
+            Hub Food
           </h1>
           <h2 className="relative left-0 mb-2 text-base font-normal text-white ml-[3%] md:ml-[11%]">
-            Loja de Roupa
+            Tráfego Pago & E-commerce
           </h2>
           <a
             data-aos="fade-right"
-            href="https://shopee.com.br/uhnstore?entryPoint=ShopBySearch&searchKeyword=uhn%20store"
+            href="https://famgz-food-hub-app.vercel.app/"
             className="bg-violet-600 mt-1 p-2 w-40 items-center justify-center rounded-full relative flex left-0 text-base font-semibold hover:bg-violet-950 transition-all text-white mt-1 ml-[3%] md:ml-[11%]"
           >
             Conferir
@@ -118,20 +116,20 @@ const RarityPage = () => {
           >
             <h1 className="w-full flex text-3xl font-bold text-black">Caso</h1>
             <p className="text-sm font-normal text-zinc-600 mt-1 hidden md:block">
-              A UHN Store começou a ser contruída em 2022 e em 2023 ficou
-              pronta! Porém, sem vendas,<br></br> com pouco engajamento e
-              estacionada, procurou por ajuda, quando nós, da Bold Company,
-              <br></br> fizemos um trabalho de tráfego pago muito bem
-              estruturado, fazendo com que as vendas e<br></br> o engajamento da
-              UHN, triplicassem...
+              A Hub Food iniciou suas atividades em 2022 com grandes
+              expectativas, mas enfrentava<br></br> desafios em 2023: vendas
+              abaixo do esperado, pouco engajamento online e dificuldade
+              <br></br> em alcançar novos clientes. Ao perceberem a necessidade
+              de uma mudança, contrataram<br></br>a Bold Company para
+              revitalizar suas estratégias de marketing digital.
             </p>
             <p className="text-sm font-normal text-zinc-600 mt-1 block md:hidden">
-              A UHN Store começou a ser construída em 2022 e em 2023{" "}
-              ficou pronta! Porém, sem vendas, com pouco engajamento e{" "}
-              estacionada, procurou por ajuda, quando nós, da Bold Company,
-               fizemos um trabalho de tráfego pago muito bem
-              estruturado, fazendo com que as vendas e o engajamento da
-              UHN, triplicassem...
+              A Hub Food iniciou suas atividades em 2022 com grandes
+              expectativas, mas enfrentava desafios em 2023: vendas abaixo do
+              esperado, pouco engajamento online e dificuldade em alcançar novos
+              clientes. Ao perceberem a necessidade de uma mudança, contrataram
+              a Bold Company para revitalizar suas estratégias de marketing
+              digital.
             </p>
           </div>
 
@@ -144,20 +142,25 @@ const RarityPage = () => {
               Solução
             </h1>
             <p className="text-sm font-normal text-zinc-600 mt-1 hidden md:inline">
-              Ao contratar a Bold Company, desenvolvemos uma estratégia de
-              tráfego pago altamente<br></br> segmentada. Essa estratégia
-              incluiu: Campanhas no Google Ads, Anúncios no Instagram e<br></br>{" "}
-              Facebook e Remarketing Inteligente para impactar visitantes que
-              acessaram o site, mas não<br></br> concluíram a compra, mostrando
-              anúncios personalizados.
+              Com a parceria da Bold Company, elaboramos uma estratégia de
+              tráfego pago personalizada<br></br> e altamente segmentada. Nossas
+              ações incluíram: Campanhas no Google Ads: com palavras-chave
+              <br></br> específicas para alimentos e delivery. Anúncios no
+              Instagram e Facebook: voltados ao público<br></br> local e com
+              conteúdos atraentes que destacavam as especialidades da Hub Food.
+              Remarketing<br></br> Inteligente: atingindo potenciais clientes
+              que visitaram o site, mas não finalizaram o pedido, por<br></br>{" "}
+              meio de anúncios persuasivos e dinâmicos.
             </p>
             <p className="text-sm font-normal text-zinc-600 mt-1 inline md:hidden">
-              Ao contratar a Bold Company, desenvolvemos uma estratégia{" "}
-              de tráfego pago altamente segmentada. Essa estratégia incluiu:
-              Campanhas no Google Ads, Anúncios no Instagram e Facebook{" "}
-              e Remarketing Inteligente para impactar visitantes que acessaram
-               o site, mas não concluíram a compra, mostrando alguns
-              anúncios personalizados.
+              Com a parceria da Bold Company, elaboramos uma estratégia de
+              tráfego pago personalizada e altamente segmentada. Nossas ações
+              incluíram: Campanhas no Google Ads: com palavras-chave específicas
+              para alimentos e delivery. Anúncios no Instagram e Facebook:
+              voltados ao público local e com conteúdos atraentes que destacavam
+              as especialidades da Hub Food. Remarketing Inteligente: atingindo
+              potenciais clientes que visitaram o site, mas não finalizaram o
+              pedido, por meio de anúncios persuasivos e dinâmicos.
             </p>
           </div>
 
@@ -176,29 +179,30 @@ const RarityPage = () => {
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
                   <span>
-                    Tráfego no Site: Crescimento de 400%, com visitantes
-                    qualificados que realmente se interessavam pelos produtos.
+                    Tráfego no Site: Crescimento de 350%, atraindo visitantes
+                    interessados nos produtos e nos serviços de delivery da Hub
+                    Food.
                   </span>
                 </p>
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
                   <span>
-                    Taxa de Conversão: Aumento de 3% para 8%, resultado da
-                    segmentação assertiva e remarketing.
+                    Taxa de Conversão: Aumento de 2,5% para 7%, graças às
+                    estratégias direcionadas e ao remarketing eficiente.
                   </span>
                 </p>
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
                   <span>
-                    Faturamento Mensal: Triplicou, ultrapassando R$ 100 mil em
-                    vendas recorrentes.
+                    Faturamento Mensal: Mais que dobrou, alcançando R$ 80 mil em
+                    vendas consistentes.
                   </span>
                 </p>
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
                   <span>
-                    Redução do Custo por Conversão: O custo caiu pela metade
-                    devido à otimização das campanhas e públicos-alvo.
+                    Redução do Custo por Conversão: O custo foi reduzido em 45%,
+                    aumentando a rentabilidade de cada pedido.
                   </span>
                 </p>
               </div>
@@ -207,21 +211,27 @@ const RarityPage = () => {
               <div className="block md:hidden">
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
-                  <span>Tráfego no Site: Crescimento de 400%</span>
-                </p>
-                <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
-                  <CircleCheckBigIcon className="text-green-600" size={17} />
-                  <span>Taxa de Conversão: Aumento de 8%</span>
-                </p>
-                <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
-                  <CircleCheckBigIcon className="text-green-600" size={17} />
                   <span>
-                    Faturamento Mensal: Triplicou.
+                    Tráfego no Site: Crescimento de 350%.
                   </span>
                 </p>
                 <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
                   <CircleCheckBigIcon className="text-green-600" size={17} />
-                  <span>Redução do Custo: Caiu pela metade</span>
+                  <span>
+                    Taxa de Conversão: Aumento de 2,5% para 7%.
+                  </span>
+                </p>
+                <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
+                  <CircleCheckBigIcon className="text-green-600" size={17} />
+                  <span>
+                    Faturamento Mensal: Mais que dobrou!
+                  </span>
+                </p>
+                <p className="flex text-base font-normal text-zinc-600 flex-row gap-2 items-center">
+                  <CircleCheckBigIcon className="text-green-600" size={17} />
+                  <span>
+                    Redução do Custo por Conversão: Reduzido em 45%.
+                  </span>
                 </p>
               </div>
             </div>
